3b636f8e09cfa363c820c20767a8b9f9baf03ae5
Jethro Williams <jethrowilliams@outlook.com>
2021-11-10 10:31:45 +0000

    Add a 'Todo' model, which is then used as the type in 'items: Todo[]'

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,11 @@
-// 'FC' stands for 'Functional Component'
-// Setting a component to have a type 'React.FC' means that it knows that it'll always have a 'children' prop (as in 'props.children')
-// Adding the `<{items: string[]}>` will then merge to this, an object with an items property, containing an array of strings
-// So 'props' is now expected to have children (optionally), and 'items'
-// (to make 'items' optional, we could instead have '<{ items?: string[] }>')
-const Todos: React.FC<{ items: string[] }> = props => {
+import Todo from '../models/todo';
+
+// This says that 'items' will be an array of 'Todo' objects
+const Todos: React.FC<{ items: Todo[] }> = props => {
   return (
     <ul>
       {props.items.map(item => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
---
+++ src/models/todo.ts
@@ -0,0 +1,12 @@
+// (could also use 'type' or 'interface' here, instead of 'class')
+class Todo {
+  id: string;
+  text: string;
+
+  constructor(todoText: string) {
+    this.text = todoText;
+    this.id = new Date().toISOString();
+  }
+}
+
+export default Todo;
