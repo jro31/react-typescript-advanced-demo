66fff8c486aaa482fa891825a420145f94cff5b2
Jethro Williams <jethrowilliams@outlook.com>
2021-11-10 11:47:06 +0000

    Use 'refs' with the <NewTodo> form, set the to-dos to a 'todos' 'state', and handle the form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo('Learn React'), new Todo('Learn Typescript')];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // This sets that the 'todos' variable will hold an array of 'Todo' objects (set in './models/todo')
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos(prevTodos => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,37 @@
-const NewTodo = () => {
-  // 'React.FormEvent' is the event type for listening to a form submission
-  // You could instead use, for example, 'React.MouseEvent', which you would get when using an 'onClick' listener
+import React, { useRef } from 'react';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = props => {
+  // This sets 'NewTodo' as a React functional component, with a prop of 'onAddTodo'
+  // It then sets the type of 'onAddTodo' as a function - Note that this does NOT create a function, it simply sets it as a function type
+  // We use 'void', because we aren't expecting any return value from the function
+  // It also sets that 'onAddTodo' will have one parameter, of type string '(text: string)'
+
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+  // This sets the ref type as being set on an <input> tag (there is also 'HTMLButtonElement', 'HTMLParagraphElement' etc)
+  // The 'null' argument sets the initial value (which is required in Typestript)
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+    // Using a '?' here (as in 'todoTextInputRef.current?.value;') would mean that if the ref is not yet connected to the <input> (so if 'current' does not exist), then it will return null
+    // (similar to using an '&' in Ruby)
+    // It sets the 'enteredText' type as 'const enteredText: string | undefined'
+    // If you instead replace the '?' with an '!' (as we're doing here), then it says that you know that 'current' will NEVER be null here
+    // It sets the 'enteredText' type as 'const enteredText: string'
+
+    if (enteredText.trim().length === 0) {
+      // Optionally throw an error here
+      return;
+    }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo text</label>
-      <input type='text' id='text' />
+      <input type='text' id='text' ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
